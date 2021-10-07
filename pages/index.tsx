2b763a136cadc40f5610887effdb3347a37f8eea
丘惟夫 <waveciou@gmail.com>
2021-10-07 23:12:35 +0800

    [dev] set up dayjs plugin

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,33 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
-const HomeComponent: React.FC = () => (
-  <div className="content">
-    <div>Home</div>
-  </div>
-);
+// Dayjs
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+// Redux
+import { RootState } from '../store';
+
+const HomeComponent: React.FC = () => {
+  dayjs.extend(utc);
+  const day = dayjs();
+  const WORDS_DATA = useSelector((state: RootState) => state.wordsCollection.value);
+  const [today, setToday] = useState<string>('');
+
+  useEffect(() => {
+    const year: number = day.utcOffset(8).year();
+    const month: number = day.utcOffset(8).month() + 1;
+    const date: number = day.utcOffset(8).date();
+    const result: string = `${year}-${month}-${date}`;
+    setToday(result);
+  }, [day]);
+
+  return (
+    <div className="content">
+      <div>{today}</div>
+    </div>
+  );
+};
 
 export default HomeComponent;
