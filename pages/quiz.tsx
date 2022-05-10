f4ba964a36958dfdcd72bfebd9426c795ecf3bbb
丘惟夫 <waveciou@gmail.com>
2022-05-10 20:47:08 +0800

    [dev] edit quiz component

--- pages/quiz.tsx
+++ pages/quiz.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/require-default-props */
 import { useRouter } from 'next/router';
 import React from 'react';
 import { useDispatch } from 'react-redux';
@@ -8,29 +9,43 @@ import { IExamId } from '@/Interfaces/exam';
 import { setIsExamAction } from '@/Slice/exam';
 
 interface IQuizzesLinkProps {
-  title: IExamId;
+  id: IExamId;
   description: string;
-  onClick: () => void;
+  isPushRoute?: boolean;
+  onClick?: () => void;
 }
 
 const QuizzesLink: React.FC<IQuizzesLinkProps> = ({
-  title = '',
+  id = '',
   description = '',
+  isPushRoute = true,
   onClick = () => {},
-}) => (
-  <div className="tw-py-5 tw-px-4 tw-rounded-lg tw-shadow-[0_1px_3px_0_rgba(51,51,51,0.4)]">
-    <div className="tw-text-wine tw-mb-5 tw-text-md tw-text-center tw-leading-9 before-font-material">
-      { getExamName(title) }
+}) => {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  return (
+    <div className="tw-py-5 tw-px-4 tw-rounded-lg tw-shadow-[0_1px_3px_0_rgba(51,51,51,0.4)]">
+      <h3 className="tw-text-wine tw-mb-5 tw-text-md tw-text-center tw-leading-9">
+        { getExamName(id) }
+      </h3>
+      <p className="tw-text-center tw-text-xs tw-text-brown">{ description }</p>
+      <div className="tw-mt-5 tw-flex tw-justify-center">
+        <PrimaryButton
+          text="開始測驗"
+          onClick={async () => {
+            if (isPushRoute) {
+              await dispatch(setIsExamAction(true));
+              await router.push(`/quiz/${id}`);
+            } else {
+              onClick();
+            }
+          }}
+        />
+      </div>
     </div>
-    <p className="tw-text-center tw-text-xs tw-text-brown">{ description }</p>
-    <div className="tw-mt-5 tw-flex tw-justify-center">
-      <PrimaryButton
-        text="開始測驗"
-        onClick={onClick}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 const Quiz: React.FC = () => {
   const router = useRouter();
@@ -40,28 +55,22 @@ const Quiz: React.FC = () => {
     <>
       <h1 className="title">單字測驗</h1>
       <div className="content">
+        <h2 className="tw-text-green-dark tw-mb-4 tw-text-lg tw-leading-9">填空測驗</h2>
         <ul>
           <li className="tw-mb-4">
             <QuizzesLink
-              title="writed-random"
+              id="writed-random"
               description="從單字資料庫隨機取得10個單字來進行填空測驗"
-              onClick={async () => {
-                await dispatch(setIsExamAction(true));
-                await router.push('/quiz/writed-random');
-              }}
             />
           </li>
           <li>
             <QuizzesLink
-              title="writed-daily"
+              id="writed-daily"
               description="使用「今日單字」進行填空測驗"
-              onClick={async () => {
-                await dispatch(setIsExamAction(true));
-                await router.push('/quiz/writed-daily');
-              }}
             />
           </li>
         </ul>
+        <h2 className="tw-text-green-dark tw-my-4 tw-text-lg tw-leading-9">選擇測驗</h2>
       </div>
     </>
   );
