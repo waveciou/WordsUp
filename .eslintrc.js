a8e1ed31b7d7b5357dffcee3ba18c254b8dde5d1
丘惟夫 <waveciou@gmail.com>
2022-06-26 17:49:41 +0800

    [dev] add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@next/next/recommended',
     'airbnb',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -18,32 +19,18 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    indent: [
-      'error',
-      2,
-      { SwitchCase: 1 },
-    ],
-    'linebreak-style': [
-      'off',
-      'unix',
-    ],
-    quotes: [
-      'error',
-      'single',
-    ],
-    semi: [
-      'error',
-      'always',
-    ],
+    // indent: ['error', 2, { SwitchCase: 1 }],
+    // 'linebreak-style': ['off', 'unix'],
+    // quotes: ['error', 'single'],
+    // semi: ['error', 'always'],
+    // 'max-len': ['off'],
     'no-unused-vars': ['off'],
     'no-undef': ['off'],
     'no-use-before-define': 'off',
     'no-underscore-dangle': 'off',
+    'object-curly-newline': ['off'],
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -52,16 +39,20 @@ module.exports = {
         tsx: 'never',
       },
     ],
-    'react/jsx-filename-extension': [1, {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    }],
-    'react/jsx-props-no-spreading': ['error', {
-      html: 'ignore',
-      custom: 'ignore',
-      exceptions: [''],
-    }],
-    'max-len': ['off'],
-    'object-curly-newline': ['off'],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'ignore',
+        custom: 'ignore',
+        exceptions: [''],
+      },
+    ],
     'react-hooks/exhaustive-deps': ['off'],
     'react/require-default-props': ['off'],
   },
